98e1115424988664f093307bb4321132299f73cc
Robson Tenório <rrtenorio@gmail.com>
2020-09-15 11:58:20 -0300

    Install Command

---
+++ src/Console/Commands/InstallCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace LighthouseDashboard\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class InstallCommand extends Command
+{
+    protected $signature = 'lighthouse-dashboard:install';
+
+    protected $description = 'Install Lighthouse Dashboard (migrations + publish assets)';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $this->warn("\n-------- Lighthouse Dashboard --------");
+        $this->call('migrate');
+        $this->callSilent('vendor:publish', [
+            '--tag' => 'lighthouse-dashboard',
+            '--force' => true
+        ]);
+        $this->info("Published fresh assets.");
+        $this->warn("--------------------------------------\n");
+    }
+}
