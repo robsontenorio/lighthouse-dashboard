8f1bfaa6315d03bc9473023b5fec3f543d176697
Robson Tenório <rrtenorio@gmail.com>
2020-09-18 22:17:45 -0300

    Remove unsed plugins

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import './plugins/moment'
 import './plugins/mask'
 import './plugins/apex-charts'
-import './plugins/highcharts'
 import vuetify from './plugins/vuetify'
 import render from './plugins/inertia'
 
--- resources/js/plugins/highcharts.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import HighchartsVue from 'highcharts-vue'
-
-Vue.use(HighchartsVue)
